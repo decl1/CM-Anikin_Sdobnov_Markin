069467e63ecd8f6dc8195e3b0a9aa60db69d2200
markinivan <markinivan2004@yandex.ru>
2024-12-15 19:21:22 +0300

    поправил неорганику

--- main/generate_data.py
+++ main/generate_data.py
@@ -8,7 +8,7 @@ def generate(batches_, stages_, sugarMin_, sugarMax_, degMin_, degMax_, concentr
         degMatrix = [[ 0 for i in range(stages_ - 1)] for j in range(batches_)]
         if concentrate_:
             for i in range(batches_):
-                const = random.uniform(0.001,(degMax_ - degMin_) / 4.0)
+                const = random.uniform(0.00000001,(degMax_ - degMin_) / 4.0)
                 constMin = random.uniform(degMin_, degMax_ - const)
                 constMax = constMin + const
                 for j in range(stages_ - 1):
@@ -20,13 +20,13 @@ def generate(batches_, stages_, sugarMin_, sugarMax_, degMin_, degMax_, concentr
         degMatrix = [[0 for i in range(stages_ - 1)] for j in range(batches_)]
         if concentrate_:
             for i in range(batches_):
-                const = random.uniform(0.001, (ripeningMax - 1.0)/4)
+                const = random.uniform(0.00000001, (ripeningMax - 1.0)/4)
                 constMin = random.uniform(1.0, ripeningMax - const)
                 constMax = constMin + const
                 for j in range(ripening_):
                     degMatrix[i][j] = random.uniform(constMin, constMax)
             for i in range(batches_):
-                const = random.uniform(0.001, (degMax_ - degMin_) / 4.0)
+                const = random.uniform(0.00000001, (degMax_ - degMin_) / 4.0)
                 constMin = random.uniform(degMin_, degMax_ - const)
                 constMax = constMin + const
                 for j in range(ripening_, stages_ - 1):
@@ -51,7 +51,7 @@ def generate(batches_, stages_, sugarMin_, sugarMax_, degMin_, degMax_, concentr
         for i in range(batches_):
             for j in range(1,stages_):
                 IMatrix[i][j] = IMatrix[i][j-1] * 1.22153980475
-        lossMatrix = [[(1.2967+0.1514*(KVector[i]+NaVector[i])+0.2159*NVector[i]+0.9989*IMatrix[i][j]) for j in range(stages_)] for i in range(batches_)]
+        lossMatrix = [[0.01*(1.2967+0.1514*(KVector[i]+NaVector[i])+0.2159*NVector[i]+0.9989*IMatrix[i][j]) for j in range(stages_)] for i in range(batches_)]
     else:
         lossMatrix = [[0 for i in range(stages_)] for j in range(batches_)]
     CMatrix = sugarMatrix
