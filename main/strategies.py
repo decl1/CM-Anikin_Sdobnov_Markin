04efb3d906ac072a8697d590978c15258f18c8ca
Пользователь <vovafigvam@gmail.com>
2025-03-17 00:09:18 +0300

    изменил стратегии

--- main/strategies.py
+++ main/strategies.py
@@ -2,9 +2,9 @@
 import sys
 
 from munkres import Munkres
+import numpy as np
 
-import generate_data as generator
-from hungarian_algorithm import algorithm
+import generate_data as g
 
 
 def output_example(input_example):
@@ -17,193 +17,197 @@ def output_example(input_example):
 # Функции возвращают число - максимальную сумму перестановки партий по этапам, полученную с помощью
 # соответствующего алгоритма, то есть S(b)
 
-def greedy_strategy(num_batches, num_stages, s_matrix):
+from munkres import Munkres
+
+def greedy_strategy(n, s_matrix):
     result = 0
     used_batches = []
-    for j in range(num_stages):
-        column = [s_matrix[i][j] for i in range(num_batches)]
+    selected_indices = []
+    
+    for j in range(n):
+        column = [s_matrix[i][j] for i in range(n)]
         for batch in used_batches:
             column[batch] = -1
         max_elem = max(column)
         result += max_elem
-        used_batches.append(column.index(max_elem))
-    return result
+        index = column.index(max_elem)
+        used_batches.append(index)
+        selected_indices.append((index, j))
+    
+    return result, selected_indices
 
 
-def thrifty_strategy(num_batches, num_stages, s_matrix):
+def thrifty_strategy(n, s_matrix):
     result = 0
     used_batches = []
-    for j in range(num_stages):
-        column = [s_matrix[i][j] for i in range(num_batches)]
+    selected_indices = []
+    
+    for j in range(n):
+        column = [s_matrix[i][j] for i in range(n)]
         for batch in used_batches:
             column[batch] = float("inf")
         min_elem = min(column)
         result += min_elem
-        used_batches.append(column.index(min_elem))
-    return result
+        index = column.index(min_elem)
+        used_batches.append(index)
+        selected_indices.append((index, j))
+    
+    return result, selected_indices
 
 
-def thrifty_greedy_strategy(num_batches, num_stages, s_matrix, swap_stage):
+def thrifty_greedy_strategy(n, s_matrix, swap_stage):
     result = 0
     used_batches = []
+    selected_indices = []
+    
     for j in range(swap_stage):
-        column = [s_matrix[i][j] for i in range(num_batches)]
+        column = [s_matrix[i][j] for i in range(n)]
         for batch in used_batches:
             column[batch] = float("inf")
         min_elem = min(column)
         result += min_elem
-        used_batches.append(column.index(min_elem))
-    for j in range(swap_stage, num_stages):
-        column = [s_matrix[i][j] for i in range(num_batches)]
+        index = column.index(min_elem)
+        used_batches.append(index)
+        selected_indices.append((index, j))
+    
+    for j in range(swap_stage, n):
+        column = [s_matrix[i][j] for i in range(n)]
         for batch in used_batches:
             column[batch] = -1
         max_elem = max(column)
         result += max_elem
-        used_batches.append(column.index(max_elem))
-    return result
+        index = column.index(max_elem)
+        used_batches.append(index)
+        selected_indices.append((index, j))
+    
+    return result, selected_indices
 
 
-def greedy_thrifty_strategy(num_batches, num_stages, s_matrix, swap_stage):
+def greedy_thrifty_strategy(n, s_matrix, swap_stage):
     result = 0
     used_batches = []
+    selected_indices = []
+    
     for j in range(swap_stage):
-        column = [s_matrix[i][j] for i in range(num_batches)]
+        column = [s_matrix[i][j] for i in range(n)]
         for batch in used_batches:
             column[batch] = -1
         max_elem = max(column)
         result += max_elem
-        used_batches.append(column.index(max_elem))
-    for j in range(swap_stage, num_stages):
-        column = [s_matrix[i][j] for i in range(num_batches)]
+        index = column.index(max_elem)
+        used_batches.append(index)
+        selected_indices.append((index, j))
+    
+    for j in range(swap_stage, n):
+        column = [s_matrix[i][j] for i in range(n)]
         for batch in used_batches:
             column[batch] = float("inf")
         min_elem = min(column)
         result += min_elem
-        used_batches.append(column.index(min_elem))
-    return result
+        index = column.index(min_elem)
+        used_batches.append(index)
+        selected_indices.append((index, j))
+    
+    return result, selected_indices
 
 
-# k < num_batches - swap_stage + 1
-def tkg_strategy(num_batches, num_stages, s_matrix, swap_stage, k):
+def tkg_strategy(n, s_matrix, swap_stage, k):
     result = 0
     used_batches = []
+    selected_indices = []
+    
     for j in range(swap_stage):
-        column = [s_matrix[i][j] for i in range(num_batches)]
+        column = [s_matrix[i][j] for i in range(n)]
         for batch in used_batches:
             column[batch] = float("inf")
         for _ in range(k):
             column[column.index(min(column))] = float("inf")
         min_elem = min(column)
         result += min_elem
-        used_batches.append(column.index(min_elem))
-
-    for j in range(swap_stage, num_stages):
-        column = [s_matrix[i][j] for i in range(num_batches)]
+        index = column.index(min_elem)
+        used_batches.append(index)
+        selected_indices.append((index, j))
+    
+    for j in range(swap_stage, n):
+        column = [s_matrix[i][j] for i in range(n)]
         for batch in used_batches:
             column[batch] = -1
         max_elem = max(column)
         result += max_elem
-        used_batches.append(column.index(max_elem))
-
-    return result
+        index = column.index(max_elem)
+        used_batches.append(index)
+        selected_indices.append((index, j))
+    
+    return result, selected_indices
 
 
 def hungarian_strategy(s_matrix):
-    # Создаем новую матрицу, в которой будем хранить результат
     new_matrix = []
-
-    # Шаг 1 и 2: Для каждой строки находим максимальный элемент и вычитаем его
-    for i in range(len(s_matrix)):  # Проходим по строкам
-        row_max = max(s_matrix[i])  # Находим максимальный элемент в строке
-        new_row = [x - row_max for x in s_matrix[i]]  # Вычитаем максимальный элемент из всех элементов строки
-        new_matrix.append(new_row)  # Добавляем измененную строку в новую матрицу
-
-    # Шаг 3: Умножаем всю новую матрицу на -1
+    
+    for i in range(len(s_matrix)):  
+        row_max = max(s_matrix[i])  
+        new_row = [x - row_max for x in s_matrix[i]]  
+        new_matrix.append(new_row)  
+    
     final_matrix = [[-x for x in row] for row in new_matrix]
 
     m = Munkres()
     indexes = m.compute(final_matrix)
+    
     total = 0
+    selected_indices = []
     for row, column in indexes:
         value = s_matrix[row][column]
         total += value
+        selected_indices.append((row, column))
+    
+    return total, selected_indices
 
-    return total
+def calculate_S1(S2, x, C):
+    x = np.array(x)  # Преобразуем x в массив numpy
+    C = np.array(C)  # Преобразуем C в матрицу numpy
+    
+    S1 = S2 + np.sum(x[:, None] * C)  # Вычисляем S1
+    return S1
 
+def run_experiment(n, ):
+    C = generate(matrix_size, base_min, base_max, hi_min, hi_max)
+    results = []
+    swap_stage = n / 3
+    k = n - swap_stage
 
-# Наша эвристическая функция. На каждом этапе выбирает партию наиболее близкую к среднему значению
-def average_strategy(num_batches, num_stages, s_matrix):
-    result = 0
-    used_batches = []
-    for j in range(num_stages):
-        # Строим текущий столбец
-        column = [s_matrix[i][j] for i in range(num_batches)]
+    algorithms = [
+        ("Greedy", greedy_strategy, (n, C)),
+        ("Thrifty", thrifty_strategy, (n, C)),
+        ("Thrifty-Greedy", thrifty_greedy_strategy, (n, C, swap_stage)),
+        ("Greedy-Thrifty", greedy_thrifty_strategy, (n, C, swap_stage)),
+        ("TKG", tkg_strategy, (n, C, swap_stage, k)),
+        ("Hungarian", hungarian_strategy, (C,))
+    ]
+    
+    hungarian_value, _ = hungarian_strategy(C)
+    min_loss = float("inf")
+    best_order = None
 
-        # Исключаем уже использованные партии
-        for batch in used_batches:
-            column[batch] = -1
+    for name, algorithm, args in algorithms:
+        value, indices = algorithm(*args)
+        S1 = calculate_S1(value, [C[i][j] for i, j in indices], C)
+        loss = hungarian_value - S1
+        results.append({
+            "Algorithm": name,
+            "Indices": indices,
+            "Sum": value,
+            "S1": S1,
+            "Loss": loss
+        })
+        if 0 < loss < min_loss:
+            min_loss = loss
+            best_order = indices
+    
+    if best_order:
+        D = generate_D(n, best_order)
+    else:
+        D = None
+    
 
-        # Вычисляем среднее арифметическое для текущего столбца
-        avg = sum(column) / len([x for x in column if x != -1])
-
-        # Находим элемент, ближайший к среднему
-        closest_value = min(column, key=lambda x: abs(x - avg) if x != -1 else float('inf'))
-
-        # Добавляем выбранный элемент в результат
-        result += closest_value
-
-        # Добавляем индекс выбранной партии в список использованных
-        used_batches.append(column.index(closest_value))
-
-    return result
-
-
-def experiment(m, d, batches, stages, swap_stage, sugarMin, sugarMax, degMin, degMax, concentrate=False, ripening=0, inoInf=False):
-    _, _, s_matrix = generator.generate(batches_=batches, stages_=stages, sugarMin_=sugarMin,
-                                        sugarMax_=sugarMax, degMin_=degMin,
-                                        degMax_=degMax, concentrate_=concentrate, ripening_=ripening,
-                                        inoInf_=inoInf)
-
-    k = stages - swap_stage
-
-    results = {strategy: {'sugar': 0, 'losses': 0} for strategy in
-               ["Greedy", "Thrifty", "Thrifty/Greedy", "Greedy/Thrifty", "T(k)G", "Average"]}
-
-    strategies = {
-        "Greedy": greedy_strategy,
-        "Thrifty": thrifty_strategy,
-        "Thrifty/Greedy": lambda b, s, sm: thrifty_greedy_strategy(b, s, sm, swap_stage),
-        "Greedy/Thrifty": lambda b, s, sm: greedy_thrifty_strategy(b, s, sm, swap_stage),
-        "T(k)G": lambda b, s, sm: tkg_strategy(b, s, sm, swap_stage, k),
-        "Average": average_strategy
-    }
-
-    max_sugar = m * d * hungarian_strategy(s_matrix)
-
-    for name, strategy in strategies.items():
-        strategy_sugar = strategy(batches, stages, s_matrix) * m * d
-        strategy_losses = max_sugar - strategy_sugar
-        results[name] = {'sugar': strategy_sugar, 'losses': strategy_losses}
-
-    return results
-
-
-def run_virtual_experiments(num_experiments, m, d, batches, stages, swap_stage, sugarMin, sugarMax, degMin, degMax,
-                            concentrate=False, ripening=0, inoInf=False):
-    total_results = {strategy: {'sugar': 0, 'losses': 0} for strategy in
-                     ["Greedy", "Thrifty", "Thrifty/Greedy", "Greedy/Thrifty", "T(k)G", "Average"]}
-
-    for _ in range(num_experiments):
-
-        experiment_results = experiment(m, d, batches, stages, swap_stage, sugarMin, sugarMax, degMin, degMax, concentrate,
-                                        ripening, inoInf)
-        for strategy, value in experiment_results.items():
-            total_results[strategy]['sugar'] += value['sugar']
-            total_results[strategy]['losses'] += value['losses']
-
-    # Усреднение результатов
-    for strategy in total_results:
-        total_results[strategy]['sugar'] /= num_experiments
-        total_results[strategy]['losses'] /= num_experiments
-
-    return total_results
+    return results, C, D
